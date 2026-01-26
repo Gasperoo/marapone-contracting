40f64014a13d036270a604fe3ff4413f74857494
Brandon Schofield <gasper@Brandons-MacBook-Air.local>
2026-01-26 18:56:40 -0500

    Update LiquidEther settings with optimized parameters

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -13,7 +13,23 @@ export default function HomePage() {
   return (
     <div className="homepage-container">
       {/* Full-screen Three.js LiquidEther Background */}
-      <LiquidEther />
+      <LiquidEther
+        colors={['#5227FF', '#FF9FFC', '#B19EEF']}
+        mouseForce={24}
+        cursorSize={100}
+        isViscous
+        viscous={30}
+        iterationsViscous={32}
+        iterationsPoisson={32}
+        resolution={0.5}
+        isBounce={false}
+        autoDemo
+        autoSpeed={0.5}
+        autoIntensity={2.2}
+        takeoverDuration={0.25}
+        autoResumeDelay={3000}
+        autoRampDuration={0.6}
+      />
 
       {/* Main Content - Overlay on top of background */}
       <div className="homepage-content">
